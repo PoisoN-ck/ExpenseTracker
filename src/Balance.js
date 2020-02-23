befbae32be4076eda9eba99d511a13616b6b1d9f
PoisoN1991 <ihor.otchenash@gmail.com>
2020-02-23 13:34:44 +0100

    Refactored code according to eslint

--- src/Balance.js
+++ src/Balance.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import MoneyRecord from './MoneyRecord';
-import Category from './Category';
 
 class Balance extends Component {
+  categories = [
+    'food',
+    'partying',
+    'clothes',
+    'flat',
+    'self-care',
+    'presents',
+    'holidays',
+    'fees',
+  ]
 
   constructor() {
     super();
@@ -11,11 +20,11 @@ class Balance extends Component {
       currentAmount: 0,
       currentCategory: this.categories.sort()[0],
       moneyFlow: [
-        {value: 100, category: "salary"},
-        {value: -20, category: "food"},
-        {value: 300, category: "salary"},
-        {value: -100, category: "clothes"}
-      ]
+        { value: 100, category: 'salary' },
+        { value: -20, category: 'food' },
+        { value: 300, category: 'salary' },
+        { value: -100, category: 'clothes' },
+      ],
     }
     this.addIncome = this.addIncome.bind(this);
     this.addExpense = this.addExpense.bind(this);
@@ -23,97 +32,94 @@ class Balance extends Component {
     this.setCurrentCategory = this.setCurrentCategory.bind(this);
   }
 
-  categories = [
-    "food",
-    "partying",
-    "clothes",
-    "flat",
-    "self-care",
-    "presents",
-    "holidays",
-    "fees",
-    "salary"
-  ]
-
-  addIncome() {
-    const {balance, currentAmount} = this.state;
-    this.addMoneyFlow(currentAmount, "salary");
-    this.setState({
-      balance: balance + currentAmount,
-      currentAmount: 0
-    });
-  }
-
-  addExpense() {
-    const {balance, currentAmount,  currentCategory} = this.state;
-    this.addMoneyFlow(currentAmount * -1, currentCategory);
-    this.setState({
-      balance: balance - currentAmount,
-      currentAmount: 0
-    });
-  }
-
   setCurrentAmount(event) {
-    const {value} = event.target;
+    const { value } = event.target;
 
-    if (isNaN(value)) {
+    if (Number.isNaN(Number(value))) {
       return;
     }
 
     this.setState({
-      currentAmount: Number(value)
+      currentAmount: Number(value),
     });
   }
 
   setCurrentCategory(event) {
-    const {value} = event.target;
+    const { value } = event.target;
 
     this.setState({
-      currentCategory: value
+      currentCategory: value,
     });
   }
 
   addMoneyFlow(value, category) {
-    const {moneyFlow} = this.state;
-    
+    const { moneyFlow } = this.state;
+
     if (!value) return;
 
     this.setState({
-      moneyFlow: [...moneyFlow, {value, category: category.charAt(0).toUpperCase() + category.slice(1)}]
+      moneyFlow: [...moneyFlow, {
+        value, category: category.charAt(0).toUpperCase() + category.slice(1),
+      }],
+    });
+  }
+
+  addExpense() {
+    const { balance, currentAmount, currentCategory } = this.state;
+
+    this.addMoneyFlow(currentAmount * -1, currentCategory);
+
+    this.setState({
+      balance: balance - currentAmount,
+      currentAmount: 0,
+    });
+  }
+
+  addIncome() {
+    const { balance, currentAmount } = this.state;
+
+    this.addMoneyFlow(currentAmount, 'Salary');
+
+    this.setState({
+      balance: balance + currentAmount,
+      currentAmount: 0,
     });
   }
 
   render() {
-    const {balance, currentAmount, currentCategory, moneyFlow} = this.state;
-    const {setCurrentAmount, setCurrentCategory, addIncome, addExpense, categories} = this;
+    const {
+      balance, currentAmount, moneyFlow,
+    } = this.state;
+
+    const {
+      setCurrentAmount, setCurrentCategory, addIncome, addExpense, categories,
+    } = this;
 
     return (
-      <React.Fragment>
-        <h1>{balance} HUF</h1>
-        <input onChange={setCurrentAmount} value={currentAmount}/>
-        <select onChange={(e) => setCurrentCategory(e)}>
+      <>
+        <h1>
+          {`${balance} HUF`}
+        </h1>
+        <input onChange={setCurrentAmount} value={currentAmount} />
+        <select onBlur={(e) => setCurrentCategory(e)}>
           {
-            categories.sort().map(item => {
-              return(
-                <option value={item}>{item.charAt(0).toUpperCase() + item.slice(1)}</option>
-              )
-            })
+            categories.sort().map(
+              (cat) => <option value={cat}>{cat.charAt(0).toUpperCase() + cat.slice(1)}</option>,
+            )
           }
         </select>
-        <button onClick={addIncome}>Add income</button>
-        <button onClick={addExpense}>Reduce income</button>
+        <button type="button" onClick={addIncome}>Add income</button>
+        <button type="button" onClick={addExpense}>Reduce income</button>
         <ul>
           {
-            moneyFlow.map((item, index) => {
-              return (
-                <MoneyRecord key={`value_${index}`} valueRecord={item.value} categoryRecord={item.category}/>
-              )
-            })
+            moneyFlow.map(
+              (item, index) => <MoneyRecord key={`val_${index + 1}`} valueRecord={item.value} categoryRecord={item.category} />,
+            )
           }
         </ul>
-      </React.Fragment>
+      </>
     );
   }
 }
 
-export default Balance
+export default Balance;
